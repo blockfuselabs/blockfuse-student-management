7736c9457713e2c830ed171882d8f9ac935b519e
morelucks <luckykamshak@gmail.com>
2025-01-11 07:32:29 +0100

    UPDATE: attendance function

--- smart_contract/contracts/BlockFuseSMS.sol
+++ smart_contract/contracts/BlockFuseSMS.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
 // Uncomment this line to use console.log
@@ -30,9 +30,53 @@ contract BlockFuseSMS {
         uint256 duration;
 
     }
-
+// Map student address to their details
 
     mapping(address => studentDetails) student;
 
+    // Attendance structure for storing attendance records
+    struct AttendanceRecord {
+        uint256 date; 
+        bool present; 
+     }
 
+    
+    mapping(uint256 => mapping(address => AttendanceRecord[])) private attendanceRecords; 
+
+    // Events
+    event AttendanceRegistered(uint256 cohortId, address indexed student, uint256 date, bool present);
+
+    // Modifier to ensure student is active
+    modifier onlyActiveStudent(address _student) {
+        require(student[_student].isActive, "Student is not active.");
+        _;
+    }
+
+    // Register attendance for a specific student
+    function registerAttendance(
+        address _student,
+        uint256 _date,
+        bool _present
+    ) external {
+        studentDetails storage student = student[_student];
+        uint256 cohortId = student.cohort;
+
+        AttendanceRecord memory record = AttendanceRecord({
+            date: _date,
+            present: _present
+        });
+
+        attendanceRecords[cohortId][_student].push(record);
+
+         emit AttendanceRegistered(cohortId, _student, _date, _present);
+
+    }
+
+    // Get attendance records for a specific student
+    function getStudentAttendance(address _student) external view returns (AttendanceRecord[] memory) {
+        studentDetails storage student = student[_student];
+        uint256 cohortId = student.cohort;
+
+        return attendanceRecords[cohortId][_student];
+    }
 }
