9e2eddf3b912b907f3dc1e5845f3672add34b821
shaaibu suleiman <shaaibusuleiman9@gmail.com>
2025-01-13 08:01:21 +0100

    chore: configure config for deployment

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -1,8 +1,37 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    // for testnet
+    "sepolia": {
+      url: process.env.SEPOLIA_RPC_URL!,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY!],
+      gasPrice: 1000000000,
+    },
+  },
+  etherscan: {
+    // Use "123" as a placeholder, because Blockscout doesn't need a real API key, and Hardhat will complain if this property isn't set.
+    apiKey: {
+      "sepolia": "123",
+    },
+    customChains: [
+      {
+        network: "sepolia",
+        chainId: 11155111,
+        urls: {
+          apiURL: "https://eth-sepolia.g.alchemy.com/v2/9-PIwmEK19yyEu468y65gQSJEIjflXjA",
+          browserURL: "https://sepolia.etherscan.io/",
+        },
+      },
+    ],
+  },
+  sourcify: {
+    enabled: false,
+  },
 };
 
 export default config;
