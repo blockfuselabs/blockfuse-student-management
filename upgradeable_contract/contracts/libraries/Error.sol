705bf4e479aa9ccf3086653d987450d44f7758bd
mananuf <bmananaf@gmail.com>
2025-01-23 18:17:51 +0100

    setup: adds LibAppStorage, Error and Event Libraries

---
+++ upgradeable_contract/contracts/libraries/Error.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+library Error {
+    // add custom errors here
+    error END_DATE_MUST_BE_GREATER_THAN_START();
+    error INVALID_COHORT_ID();
+    error COHORT_DOES_NOT_EXIST();
+    error UNAUTHORIZED_ACCESS();
+    error STUDENT_DOES_NOT_EXIST();
+    error INVALID_TRACK();
+    error TRACK_DOES_NOT_EXIST_IN_COHORT();
+    error STUDENT_IS_NOT_ACTIVE();
+    error ALREADY_MARKED_ATTENDANCE();
+}
